573befcceb2b0f0063d85a7bb23a6607bc5df629
jamesoyanna <engrjayt200@gmail.com>
2023-05-05 23:48:20 +0100

    Initiate databse connection

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+require("dotenv").config()
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Hello Api");
-})
+});
 
-const PORT = 5000;
+/* connect to database */
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log("Database Connected Successfully..."))
+.catch(err=>console.log(err))
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT , () => {
     console.log(`Server listening on ${PORT}`);
 });
