e13a2d7197ebcb8999b08c0917539aa5e2956bee
jamesoyanna <engrjayt200@gmail.com>
2023-05-24 16:10:47 +0100

    Bug fix

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const connectToDatabase = require("./db.js");
+const connectToDatabase = require("./db.js")
 const { Novu } = require('@novu/node');
-const puppeteer = require('puppeteer');
+const pdf = require('html-pdf');
 const cors = require('cors');
 const fs = require('fs');
 
-const pdfTemplate = require('./documents/index.js');
+const pdfTemplate = require('./documents/index.js')
 
 const app = express();
 
-require("dotenv").config();
+require("dotenv").config()
 
 app.use(express.json());
 
@@ -21,7 +21,7 @@ connectToDatabase();
 
 // Routes
 const userRoutes = require('./routes/userRoutes.js');
-const invoiceRoutes = require('./routes/invoiceRoute.js');
+const invoiceRoutes = require('./routes/invoiceRoute.js')
 
 app.use('/api/user', userRoutes);
 app.use('/api/invoices', invoiceRoutes);
@@ -32,38 +32,28 @@ app.get('/', (req, res) => {
 
 let options = { format: 'A4' };
 
-app.post('/send-pdf', async (req, res) => {
+app.post('/send-pdf', (req, res) => {
   const { email } = req.body;
-
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    // Generate PDF content using pdfTemplate function
-    const content = pdfTemplate(req.body);
-
-    // Set the HTML content of the page
-    await page.setContent(content);
-
-    // Generate the PDF
-    const pdfBuffer = await page.pdf({ format: 'A4' });
-
-    // Close the browser
-    await browser.close();
+  pdf.create(pdfTemplate(req.body), options).toFile('invoice.pdf', (err) => {
+    if (err) {
+      // Handle any error that occurred during PDF generation
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while generating the PDF.' });
+    }
 
     const novu = new Novu(process.env.NOVU_API_KEY);
 
     novu.trigger('invoice-notification', {
       to: {
         subscriberId: '63695b559e04bb11b56924df',
-        email: email
+        email: `${email}`
       },
       payload: {
         attachments: [
           {
-            file: pdfBuffer.toString('base64'),
+            file: fs.readFileSync(__dirname + '/invoice.pdf').toString('base64'),
             name: 'invoice.pdf',
-            mime: 'application/pdf',
+            mime: 'application/octet-stream',
           },
         ],
       },
@@ -78,44 +68,28 @@ app.post('/send-pdf', async (req, res) => {
 
     // Send the response with the data
     res.json(responseData);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'An error occurred while generating the PDF.' });
-  }
+  });
 });
 
-app.post('/create-pdf', async (req, res) => {
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
 
-    // Generate PDF content using pdfTemplate function
-    const content = pdfTemplate(req.body);
 
-    // Set the HTML content of the page
-    await page.setContent(content);
-
-    // Generate the PDF
-    const pdfBuffer = await page.pdf({ format: 'A4' });
-
-    // Close the browser
-    await browser.close();
-
-    // Save the PDF to a file
-    fs.writeFileSync('invoice.pdf', pdfBuffer);
-
-    res.send('PDF created successfully.');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('An error occurred while generating the PDF.');
-  }
+//CREATE AND SEND PDF INVOICE
+app.post('/create-pdf', (req, res) => {
+  pdf.create(pdfTemplate(req.body), {}).toFile('invoice.pdf', (err) => {
+      if(err) {
+          res.send(Promise.reject());
+      }
+      res.send(Promise.resolve());
+  });
 });
 
+//SEND PDF INVOICE
 app.get('/fetch-pdf', (req, res) => {
-  res.sendFile(`${__dirname}/invoice.pdf`);
-});
+   res.sendFile(`${__dirname}/invoice.pdf`)
+})
+
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+app.listen(PORT , () => {
+    console.log(`Server listening on ${PORT}`);
 });
